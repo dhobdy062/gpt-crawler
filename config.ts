b94d0a155d02992dccb90816c27d9d09804068a7
Donchalant <56202953+dhobdy062@users.noreply.github.com>
2025-02-22 01:56:05 -0600

    adjust cobfig file

--- config.ts
+++ config.ts
@@ -2,7 +2,7 @@ import { Config } from "./src/config";
 
 export const defaultConfig: Config = {
   url: "https://vercel.com/docs/integrations/ai",
-  match: "https://vercel.com/docs/integrations/ai",
+  match: "https://vercel.com/docs/integrations/ai/**",
   maxPagesToCrawl: 109,
   outputFileName: "output.json",
   maxTokens: 500000,
